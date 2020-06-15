4ac8bbd01d4aebe192464ab2df07568bd25cc4ff
ieva ozolite <ieva.ozolite@gmail.com>
2020-06-15 21:43:48 +0300

    create status component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <StatusItem msg="Welcome to Your Vue.js App" />
+    <StatusItem currentStatus="test" />
   </div>
 </template>
 
--- src/components/StatusItem.vue
+++ src/components/StatusItem.vue
@@ -1,12 +1,76 @@
 <template>
-  <h1>task name</h1>
+  <aside class="status">
+    <button @click="openStatusList" class="status--button">{{ status }}</button>
+    <form @submit="getStatus" class="status-list">
+      <fieldset>
+        <legend class="sr-only">Change status</legend>
+        <div class="status-list--item">
+          <input
+            type="radio"
+            value="waiting"
+            id="1"
+            v-model="status"
+            name="status"
+          />
+          <label for="1">Waiting</label>
+        </div>
+        <div class="status-list--item">
+          <input
+            type="radio"
+            id="2"
+            name="status"
+            value="test"
+            v-model="status"
+          />
+          <label for="2">Test</label>
+        </div>
+        <div class="status-list--item">
+          <input
+            type="radio"
+            id="3"
+            name="status"
+            v-model="status"
+            value="ready"
+          />
+          <label for="3">Ready</label>
+        </div>
+      </fieldset>
+    </form>
+    <!-- <div>
+      <input
+        type="radio"
+        value="One"
+        :checked="picked == 'One'"
+        @change="e => (picked = e.target.value)"
+      />
+      <input
+        type="radio"
+        value="Two"
+        :checked="picked == 'Two'"
+        @change="e => (picked = e.target.value)"
+      />
+      <span>Picked: {{ picked }}</span>
+    </div> -->
+  </aside>
 </template>
 
 <script>
 export default {
   name: "StatusItem",
+  data() {
+    return {
+      status: this.currentStatus
+      //picked: ""
+    };
+  },
   props: {
-    msg: String
+    currentStatus: String
+  },
+  methods: {
+    openStatusList() {},
+    getStatus() {
+      console.log(this.status);
+    }
   }
 };
 </script>
